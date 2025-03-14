e27805c2034e7ddf679ba61bf9de72223f4ea445
traorecheikh <atidiane741@gmail.com>
2025-03-14 23:09:01 +0000

    test

---
+++ database/migrations/2025_03_14_230453_create_commandes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->enum('statut', ['en attente', 'en preparation', 'prete', 'payee'])->default('en attente');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commandes');
+    }
+};
---
+++ database/migrations/2025_03_14_230542_create_paiements_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('paiements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('commande_id')->constrained();
+            $table->date('date_paiement')->nullable();
+            $table->decimal('montant', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('paiements');
+    }
+};
