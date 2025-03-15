48861ef097e0c92bb177d89c96bbdd6b29d634d2
traorecheikh <atidiane741@gmail.com>
2025-03-15 02:51:15 +0000

    hunk code

--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Commande extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'total', 'statut'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function burgers()
+    {
+        return $this->belongsToMany(Burger::class, 'commande_burger')->withPivot('quantite');
+    }
 }
