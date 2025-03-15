cad59257d135887afc6fdf986c21b460a3bf21ac
traorecheikh <atidiane741@gmail.com>
2025-03-15 02:51:02 +0000

    hunk code

--- app/Models/Burger.php
+++ app/Models/Burger.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Burger extends Model
 {
-    //
+
+    use HasFactory;
+
+    protected $fillable = ['nom', 'prix', 'image', 'description', 'stock'];
+
+    public function commandes()
+    {
+        return $this->belongsToMany(Commande::class, 'commande_burger')->withPivot('quantite');
+    }
 }
