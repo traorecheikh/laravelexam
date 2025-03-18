1a98b10a7707593cad5432707364809a8488bb61
traorecheikh <atidiane741@gmail.com>
2025-03-18 21:44:08 +0000

    add some files

--- resources/views/panier/show.blade.php
+++ resources/views/panier/show.blade.php
@@ -1,5 +1,7 @@
 <form action="{{ route('panier.ajouter') }}" method="POST">
     @csrf
     <input type="hidden" name="burger_id" value="{{ $burger->id }}">
-    <button type="submit" class="btn btn-success">Ajouter au Panier</button>
+    @cannot('gerer burgers')
+        <button type="submit" class="btn btn-success">Ajouter au Panier</button>
+    @endcannot
 </form>
